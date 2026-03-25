39182cea2b0d810b83c138378710e67b8ed14ac3
deadvoxelx <deadvoxelx@gmail.com>
2026-03-25 10:40:58 -0400

    Update LivingEntity.cpp

--- Minecraft.World/LivingEntity.cpp
+++ Minecraft.World/LivingEntity.cpp
@@ -986,9 +986,33 @@ bool LivingEntity::onLadder()
 	int yt = Mth::floor(bb->y0);
 	int zt = Mth::floor(z);
 
-	// 4J-PB - TU9 - add climbable vines
 	int iTile = level->getTile(xt, yt, zt);
-	return  (iTile== Tile::ladder_Id) || (iTile== Tile::vine_Id);
+	switch (iTile)
+	{
+	case Tile::ladder_Id:
+	case Tile::vine_Id: // 4J-PB - TU9 - add climbable vines
+	case Tile::netherVine_Id:
+		return true;
+	case Tile::trapdoor_Id: // hexagonny - add climbable (opened) trapdoors
+	{
+		if ((level->getData(xt, yt, zt) & 0x4) != 0)
+		{
+			switch (level->getTile(xt, yt + 1, zt))
+			{
+			case Tile::ladder_Id:
+			case Tile::vine_Id:
+			case Tile::netherVine_Id:
+				return false; // Opened trapdoor should only be climbable when it's only at the top.
+			default:
+				return level->getTile(xt, yt - 1, zt) == Tile::ladder_Id;
+			}
+		}
+		break;
+	}
+	default:
+		break;
+	}
+	return false;
 }
 
 bool LivingEntity::isShootable()

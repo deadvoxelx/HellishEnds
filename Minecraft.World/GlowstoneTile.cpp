e599c442513d44d71cbeec286f852c03ea7107a2
DeadVoxelx <deadvoxelx@gmail.com>
2026-03-28 13:38:05 -0400

    Glowstone Improvements
    Always drops 4 Glowstone Dust
    Can be efficiently mined with a Pickaxe

--- Minecraft.World/GlowstoneTile.cpp
+++ Minecraft.World/GlowstoneTile.cpp
@@ -6,14 +6,9 @@ Glowstonetile::Glowstonetile(int id, Material *material) : Tile(id, material)
 {
 }
 
-int Glowstonetile::getResourceCountForLootBonus(int bonusLevel, Random *random)
-{
-	return Mth::clamp(getResourceCount(random) + random->nextInt(bonusLevel + 1), 1, 4);
-}
-
 int Glowstonetile::getResourceCount(Random *random)
 {
-	return 2 + random->nextInt(3);
+	return 4;
 }
 
 int Glowstonetile::getResource(int data, Random *random, int playerBonusLevel)

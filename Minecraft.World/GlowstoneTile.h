6e58710e6c8bbba487b64b78c424bc00d52e1dab
DeadVoxelx <deadvoxelx@gmail.com>
2026-03-28 13:36:54 -0400

    Update GlowstoneTile.h

--- Minecraft.World/GlowstoneTile.h
+++ Minecraft.World/GlowstoneTile.h
@@ -7,7 +7,6 @@ class Glowstonetile : public Tile
 {
 public:
     Glowstonetile(int id, Material *material);
-    virtual int getResourceCountForLootBonus(int bonusLevel, Random *random);
     virtual int getResourceCount(Random *random);
     virtual int getResource(int data, Random *random, int playerBonusLevel);
 };

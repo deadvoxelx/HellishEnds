51ed2dfa9e88673a509fa1ca42939f3cd52e9b4d
DeadVoxelx <deadvoxelx@gmail.com>
2026-04-09 15:57:16 -0400

    Update Dead Bush

--- Minecraft.World/DeadBushTile.h
+++ Minecraft.World/DeadBushTile.h
@@ -9,11 +9,13 @@ class DeadBushTile : public Bush
 	friend class Tile;
 protected:
 	DeadBushTile(int id);
-
+	virtual shared_ptr<ItemInstance> getSilkTouchItemInstance(int data);
 public:
 	virtual bool canSurvive(Level *level, int x, int y, int z);
 	virtual bool mayPlace(Level *level, int x, int y, int z);
-    virtual void updateDefaultShape(); // 4J Added override
+    virtual void updateDefaultShape();
     virtual int getResource(int data, Random *random, int playerBonusLevel);
-	virtual void playerDestroy(Level *level, shared_ptr<Player> player, int x, int y, int z, int data);
+	virtual int getResourceCount(Random *random);
+	virtual int getResourceCountForLootBonus(int bonusLevel, Random *random);
+	virtual void spawnResources(Level *level, int x, int y, int z, int data, float odds, int playerBonusLevel);
 };

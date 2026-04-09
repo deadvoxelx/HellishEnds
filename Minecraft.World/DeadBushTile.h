2d2fe00a71dba8885f515d76c37a614885d8a2fe
DeadVoxelx <deadvoxelx@gmail.com>
2026-04-09 13:24:15 -0400

    End Update Content

--- Minecraft.World/DeadBushTile.h
+++ Minecraft.World/DeadBushTile.h
@@ -2,14 +2,17 @@
 #include "Bush.h"
 
 class Random;
+class Level;
 
 class DeadBushTile : public Bush
 {
 	friend class Tile;
 protected:
 	DeadBushTile(int id);
-    virtual bool mayPlaceOn(int tile);
+
 public:
+	virtual bool canSurvive(Level *level, int x, int y, int z);
+	virtual bool mayPlace(Level *level, int x, int y, int z);
     virtual void updateDefaultShape(); // 4J Added override
     virtual int getResource(int data, Random *random, int playerBonusLevel);
 	virtual void playerDestroy(Level *level, shared_ptr<Player> player, int x, int y, int z, int data);

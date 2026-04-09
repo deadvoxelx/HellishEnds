29b2efad8946dcf934ec9d16e7cf82c2dc52da04
DeadVoxelx <deadvoxelx@gmail.com>
2026-04-09 19:48:24 -0400

    Fix Veloett Vine Dropping Stone

--- Minecraft.World/VeloettVineTile.cpp
+++ Minecraft.World/VeloettVineTile.cpp
@@ -1,6 +1,5 @@
 #include "stdafx.h"
 #include "net.minecraft.stats.h"
-#include "net.minecraft.world.entity.player.h"
 #include "net.minecraft.world.item.h"
 #include "net.minecraft.world.level.h"
 #include "VeloettVineTile.h"
@@ -27,15 +26,4 @@ bool VeloettVineTile::canSurvive(Level *level, int x, int y, int z)
 {
 	int below = level->getTile(x, y - 1, z);
 	return below == Tile::veloettVine_Id || below == Tile::veloettGrass_Id;
-}
-
-int VeloettVineTile::getResource(int data, Random *random, int playerBonusLevel)
-{
-	return 1;
-}
-
-void VeloettVineTile::playerDestroy(Level *level, shared_ptr<Player> player, int x, int y, int z, int data)
-{
-	VeloettVineTile::playerDestroy(level, player, x, y, z, data);
-	popResource(level, x, y, z, std::make_shared<ItemInstance>(Tile::veloettVine, 1));
 }

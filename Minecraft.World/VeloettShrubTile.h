872ec796d5be71013fa6c1cf05445004e74a6882
DeadVoxelx <deadvoxelx@gmail.com>
2026-04-09 20:12:49 -0400

    Make Veloett Shrub Drop Veloett Berry

--- Minecraft.World/VeloettShrubTile.h
+++ Minecraft.World/VeloettShrubTile.h
@@ -9,8 +9,11 @@ class VeloettShrubTile : public Bush
 protected:
 	VeloettShrubTile(int id);
     virtual bool mayPlaceOn(int tile);
+	virtual shared_ptr<ItemInstance> getSilkTouchItemInstance(int data);
 public:
     virtual void updateDefaultShape(); // 4J Added override
     virtual int getResource(int data, Random *random, int playerBonusLevel);
-	virtual void playerDestroy(Level *level, shared_ptr<Player> player, int x, int y, int z, int data);
+	virtual int getResourceCount(Random *random);
+	virtual int getResourceCountForLootBonus(int bonusLevel, Random *random);
+	virtual void spawnResources(Level *level, int x, int y, int z, int data, float odds, int playerBonusLevel);
 };

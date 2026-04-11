f1ebc351f608b8b46a44a71c5e1de37bffe93c57
DeadVoxelx <deadvoxelx@gmail.com>
2026-04-11 18:44:38 -0400

    Update StructureRecipies.cpp

--- Minecraft.World/StructureRecipies.cpp
+++ Minecraft.World/StructureRecipies.cpp
@@ -83,6 +83,15 @@ void StructureRecipies::addRecipes(Recipes *r)
 		L'#', Tile::goldenclin,
 		L'S');
 
+	r->addShapedRecipy(new ItemInstance(Tile::furnace), //
+		L"sssctg",
+		L"###", //
+		L"# #", //
+		L"###", //
+
+		L'#', Tile::endStone,
+		L'S');
+
 	r->addShapedRecipy(new ItemInstance(Tile::nether_furnace), //
 		L"sssctg",
 		L"###", //

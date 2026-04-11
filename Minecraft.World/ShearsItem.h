ba22fd1618f9a7601262155035d219e5f4024abe
DeadVoxelx <deadvoxelx@gmail.com>
2026-04-10 21:11:33 -0400

    Update ShearsItem.h
    shear pumpkins for carved pumpkins

--- Minecraft.World/ShearsItem.h
+++ Minecraft.World/ShearsItem.h
@@ -3,6 +3,8 @@ using namespace std;
 
 #include "Item.h"
 
+class Player;
+
 class ShearsItem : public Item 
 {
 public:
@@ -10,4 +12,5 @@ public:
 	virtual bool mineBlock(shared_ptr<ItemInstance> itemInstance, Level *level, int tile, int x, int y, int z, shared_ptr<LivingEntity> owner);
 	virtual bool canDestroySpecial(Tile *tile);
 	virtual float getDestroySpeed(shared_ptr<ItemInstance> itemInstance, Tile *tile);
+	virtual bool useOn(shared_ptr<ItemInstance> instance, shared_ptr<Player> player, Level *level, int x, int y, int z, int face, float clickX, float clickY, float clickZ, bool bTestUseOnOnly=false);
 };

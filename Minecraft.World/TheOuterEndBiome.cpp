6080e029fc8f6e74ecbd74386c7c2166fcee27ed
DeadVoxelx <deadvoxelx@gmail.com>
2026-04-12 16:20:14 -0400

    Add White Enderman Spawning

--- Minecraft.World/TheOuterEndBiome.cpp
+++ Minecraft.World/TheOuterEndBiome.cpp
@@ -14,6 +14,7 @@ TheOuterEndBiome::TheOuterEndBiome(int id) : Biome(id)
 
 	enemies.push_back(new MobSpawnerData(eTYPE_WATCHER, 7, 1, 1));
 	enemies.push_back(new MobSpawnerData(eTYPE_ENDERMAN, 10, 4, 4));
+	enemies.push_back(new MobSpawnerData(eTYPE_WHITE_ENDERMAN, 4, 4, 4));
 
 	decorator = new TheOuterEndBiomeDecorator(this);
 }
